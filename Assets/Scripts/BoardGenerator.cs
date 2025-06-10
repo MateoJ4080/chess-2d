c3739ae4d6bd0fb197474bdd889679c6aaef4b0b
Mate93934 <mateodjm@gmail.com>
2025-06-10 20:42:30 -0300

    chore: remove _piecesContainer variable and its positioning
    - Not necessary for now, since now the camera is the one being positioned instead of the board and the pieces themselves

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -5,7 +5,6 @@ public class BoardGenerator : MonoBehaviour
     [SerializeField] private GameObject _squarePrefab; // Square sprite with SpriteRenderer
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private Transform _squaresContainer;
-    [SerializeField] private Transform _piecesContainer;
     [SerializeField] private Color _lightColor = Color.white;
     [SerializeField] private Color _darkColor = Color.gray;
 
@@ -34,7 +33,6 @@ public class BoardGenerator : MonoBehaviour
         }
         // "0.5f" as offset to center the board, because the tiles are positioned by their left-bottom corner
         _mainCamera.transform.position = new Vector3(_boardWidth / 2f - 0.5f, _boardHeight / 2f - 0.5f, -10f);
-        _piecesContainer.transform.position = _squaresContainer.transform.position;
         // OnBoardGenerated?.invoke();
     }
 }
