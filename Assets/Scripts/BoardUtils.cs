5e65af88b5e04488ddb439f4eca50ea784bdb210
Mate93934 <mateodjm@gmail.com>
2025-06-20 23:53:33 -0300

    feat: extend BoardUtils with capture logic and position queries
    - Add CanCaptureAt method for piece capture logic
    - Add GetPieceAt method for position queries
    - Rename SquareIsAvailable to SquareIsEmpty for clarity

--- Assets/Scripts/BoardUtils.cs
+++ Assets/Scripts/BoardUtils.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public static class BoardUtils
 {
-    public static bool SquareIsAvailable(Vector2Int position)
+    public static bool SquareIsEmpty(Vector2Int position)
     {
         return BoardGenerator.Instance.Squares.ContainsKey(position) &&
                 !BoardGenerator.Instance.PiecesOnBoard.ContainsValue(position);
+    }
+
+    public static bool CanCaptureAt(Vector2Int position, GameObject movedPiece)
+    {
+        if (!SquareIsEmpty(position))
+        {
+            GameObject capturedPiece = GetPieceAt(position);
+            if (movedPiece != null && capturedPiece != null)
+            {
+                Debug.Log($"Pieces: {movedPiece}, {capturedPiece}");
+                bool movedPieceColor = movedPiece.GetComponent<ChessPiece>().PieceData.IsWhite;
+                bool capturedPieceColor = capturedPiece.GetComponent<ChessPiece>().PieceData.IsWhite;
+                return movedPieceColor != capturedPieceColor;
+            }
+        }
+        return false;
+    }
+
+    public static GameObject GetPieceAt(Vector2Int position)
+    {
+        if (BoardGenerator.Instance.PositionToPiece.TryGetValue(position, out GameObject piece)) return piece;
+        return null;
     }
 }
--- Assets/Scripts/ChessPiece.cs
+++ Assets/Scripts/ChessPiece.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ChessPiece : MonoBehaviour
 {
+    // PieceData is asigned at PieceSpawner.cs
     private PieceData pieceData;
     public PieceData PieceData { get => pieceData; set => pieceData = value; }
-
 }
