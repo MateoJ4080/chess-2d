c1a02f322d7b9b70265d93f7a761025e03e8f98d
Mate93934 <mateodjm@gmail.com>
2026-02-11 20:54:08 -0300

    feat: improve debug UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,4 +1,4 @@
-using System;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +7,17 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _loadingMenu;
+
+    // Debug texts
     [SerializeField] private TextMeshProUGUI _networkStatusText;
+    [SerializeField] private TextMeshProUGUI _debugTextState;
+    [SerializeField] private TextMeshProUGUI _debugTextConnection;
+    [SerializeField] private TextMeshProUGUI _debugTextReady;
+    [SerializeField] private TextMeshProUGUI _debugTextLobby;
+    [SerializeField] private TextMeshProUGUI _debugTextRoom;
+    [SerializeField] private TextMeshProUGUI _debugTextMaster;
+    [SerializeField] private TextMeshProUGUI _debugTextScene;
+
 
     public static UIManager Instance { get; private set; }
 
@@ -19,11 +29,37 @@ public class UIManager : MonoBehaviour
             return;
         }
         Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            _loadingMenu.SetActive(false);
+            _networkStatusText.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
 
     void Start()
     {
         ShowMainMenu();
+    }
+
+    void Update()
+    {
+        UpdateDebugNetworkTexts();
     }
 
     public void ShowMainMenu()
@@ -34,6 +70,7 @@ public class UIManager : MonoBehaviour
 
     public void ShowLoadingMenu()
     {
+        _mainMenu.SetActive(false);
         _loadingMenu.SetActive(true);
     }
 
@@ -41,5 +78,15 @@ public class UIManager : MonoBehaviour
     {
         _networkStatusText.text = text;
     }
+
+    void UpdateDebugNetworkTexts()
+    {
+        _debugTextState.text = $"State: {PhotonNetwork.NetworkClientState}";
+        _debugTextConnection.text = $"Connection: {PhotonNetwork.IsConnected}";
+        _debugTextReady.text = $"Ready: {PhotonNetwork.IsConnectedAndReady}";
+        _debugTextLobby.text = $"Lobby: {PhotonNetwork.InLobby}";
+        _debugTextRoom.text = $"Room: {PhotonNetwork.InRoom}";
+        _debugTextMaster.text = $"Master: {PhotonNetwork.IsMasterClient}";
+        _debugTextScene.text = $"Scene: {SceneManager.GetActiveScene().name}";
     }
 }
