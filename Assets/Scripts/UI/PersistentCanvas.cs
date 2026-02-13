621d974d51f13255af1a43f3e4bc22ba45135df8
Mate93934 <mateodjm@gmail.com>
2026-02-12 22:14:08 -0300

    fix(PersistentCanvas): prevent duplicate instances

--- Assets/Scripts/UI/PersistentCanvas.cs
+++ Assets/Scripts/UI/PersistentCanvas.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class PersistentCanvas : MonoBehaviour
 {
+    public static PersistentCanvas Instance;
+
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 }
