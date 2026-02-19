c8c66913fe9dfbdb81cb6bda1361ecaec9138bef
Mate93934 <mateodjm@gmail.com>
2026-02-19 02:20:03 -0300

    chore(PlayerManager): general cleanup

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private TextMeshProUGUI colorInfoText;
     private bool colorsAreAssigned;
     public bool ColorsAreAssigned
     {
@@ -50,42 +49,35 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     {
         Player[] players = PhotonNetwork.PlayerList;
 
-        Debug.Log($"[AssignRandomColors] | Players in room: {players.Length}");
-
         if (players.Length < 2)
         {
-            Debug.LogWarning("Not enough players to assign colors!");
+            Debug.LogWarning("Not enough players to assign colors");
             return;
         }
 
-        if (!PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogWarning("Only the MasterClient should assign colors!");
-            return;
-        }
+        if (!PhotonNetwork.IsMasterClient) return;
 
         Debug.Log("Assigning colors to players...");
         bool firstIsWhite = Random.value < 0.5f;
 
-        ExitGames.Client.Photon.Hashtable p1props = new();
+        Hashtable p1props = new();
         p1props["Color"] = firstIsWhite ? "White" : "Black";
         players[0].SetCustomProperties(p1props);
 
-        ExitGames.Client.Photon.Hashtable p2props = new();
+        Hashtable p2props = new();
         p2props["Color"] = firstIsWhite ? "Black" : "White";
         players[1].SetCustomProperties(p2props);
 
         // Set room property to indicate colors are assigned
-        ExitGames.Client.Photon.Hashtable roomProps = new();
+        Hashtable roomProps = new();
         roomProps["ColorsAssigned"] = true;
         PhotonNetwork.CurrentRoom.SetCustomProperties(roomProps);
 
         Instance.ColorsAreAssigned = true;
-        Debug.Log("Colors assigned successfully!");
     }
 
     // Called when room properties change
-    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
     {
         if (propertiesThatChanged.ContainsKey("ColorsAssigned"))
         {
