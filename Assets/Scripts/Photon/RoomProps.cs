9134dbfad2880d044e4d050075de7a9c174b637d
Mate93934 <mateodjm@gmail.com>
2026-03-29 00:21:08 -0300

    feat: add RoomProps

---
+++ Assets/Scripts/Photon/RoomProps.cs
@@ -0,0 +1,8 @@
+public static class RoomProps
+{
+    public const string P1Name = "p1";
+    public const string P2Name = "p2";
+    public const string P1Elo = "e1";
+    public const string P2Elo = "e2";
+    public const string MatchTime = "t";
+}
