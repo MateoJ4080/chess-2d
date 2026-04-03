f5f8f747ca1001d3b0c2c8599c82a32e610b610d
Mate93934 <mateodjm@gmail.com>
2026-04-02 21:25:29 -0300

    feat: allow player to join room as spectator

--- Assets/Scripts/UI/MatchItem.cs
+++ Assets/Scripts/UI/MatchItem.cs
@@ -1,15 +1,19 @@
 using TMPro;
 using UnityEngine;
 using Photon.Realtime;
+using Photon.Pun;
 
 public class MatchItem : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI player1Text;
     [SerializeField] private TextMeshProUGUI player2Text;
     [SerializeField] private TextMeshProUGUI timeText;
+    private string _roomName;
+
 
     public void SetData(RoomInfo room)
     {
+        _roomName = room.Name;
         player1Text.text = $"{GetProp(room, RoomProps.P1Name)} ({GetProp(room, RoomProps.P1Elo)})";
         player2Text.text = $"{GetProp(room, RoomProps.P2Name)} ({GetProp(room, RoomProps.P2Elo)})";
     }
@@ -24,5 +28,14 @@ public class MatchItem : MonoBehaviour
     {
         player1Text.text = $"{p1} ({e1})";
         player2Text.text = $"{p2} ({e2})";
+    }
+
+    public void JoinRoomByButton()
+    {
+        if (!PhotonNetwork.InLobby) return;
+
+        UIManager.Instance.HideMatchList();
+        Debug.Log($"JoinRoomByButton: trying joining to {_roomName}");
+        PhotonNetwork.JoinRoom(_roomName);
     }
 }
