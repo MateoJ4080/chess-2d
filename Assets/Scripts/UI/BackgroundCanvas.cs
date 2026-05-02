3bf8861cc56ddd60330e94f586be3bee97412d81
Mate93934 <mateodjm@gmail.com>
2026-05-02 06:40:14 -0300

    refactor(ui): separate background into its own canvas

---
+++ Assets/Scripts/UI/BackgroundCanvas.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BackgroundCanvas : MonoBehaviour
+{
+    public static BackgroundCanvas Instance;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
