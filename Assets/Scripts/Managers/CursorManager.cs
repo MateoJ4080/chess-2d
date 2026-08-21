f962d327ae0e7759b91fb1d93645751346dfdd71
Mate93934 <mateodjm@gmail.com>
2026-08-21 13:24:29 -0300

    refactor: restructure CursorManager

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -1,57 +1,86 @@
+using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.InteropServices;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CursorManager : MonoBehaviour
 {
-    private Camera _cam;
+    public static CursorManager Instance { get; private set; }
 
     [SerializeField] private Texture2D _defaultCursor;
     [SerializeField] private Texture2D _handCursor;
     [SerializeField] private Vector2 _defaultHotspot;
     [SerializeField] private Vector2 _handHotspot;
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-    [DllImport("__Internal")] private static extern void SetCursorPointer();
-    [DllImport("__Internal")] private static extern void SetCursorDefault();
-#endif
+    private Camera _cam;
 
-    void Awake()
+    private void Awake()
     {
-        _cam = Camera.main;
-    }
-
-    void Update()
-    {
-        Vector3 mouse = Input.mousePosition;
-
-        if (mouse.x < 0 || mouse.y < 0 ||
-            mouse.x > Screen.width || mouse.y > Screen.height)
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
             return;
+        }
 
-        Vector2 mousePos = _cam.ScreenToWorldPoint(mouse);
-        Collider2D hit = Physics2D.OverlapPoint(mousePos);
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
 
-        if (hit != null && hit.CompareTag("Piece"))
-            SetHand();
+        _cam = Camera.main;
+        SetCursor(CursorType.Default);
+    }
+
+    private void Update()
+    {
+        if (IsHoveringButton() || IsHoveringPiece())
+            SetCursor(CursorType.Hand);
         else
-            SetDefault();
+            SetCursor(CursorType.Default);
     }
 
-    void SetHand()
+    private bool IsHoveringButton()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        SetCursorPointer();
-#else
-        Cursor.SetCursor(_handCursor, _handHotspot, CursorMode.Auto);
-#endif
+        PointerEventData pointerData = new(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.TryGetComponent<Button>(out _))
+                return true;
+        }
+
+        return false;
     }
 
-    void SetDefault()
+    private bool IsHoveringPiece()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        SetCursorDefault();
-#else
-        Cursor.SetCursor(_defaultCursor, _defaultHotspot, CursorMode.Auto);
-#endif
+        Vector2 mousePosition = _cam.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D hit = Physics2D.OverlapPoint(mousePosition);
+
+        return hit != null && hit.CompareTag("Piece");
     }
+
+    public void SetCursor(CursorType type)
+    {
+        switch (type)
+        {
+            case CursorType.Hand:
+                Cursor.SetCursor(_handCursor, _handHotspot, CursorMode.Auto);
+                break;
+
+            default:
+                Cursor.SetCursor(_defaultCursor, _defaultHotspot, CursorMode.Auto);
+                break;
+        }
+    }
+}
+
+public enum CursorType
+{
+    Default,
+    Hand
 }
